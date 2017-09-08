50c900bf941296cabf150fcc1bd8f9b2696210cd
Mark Alston <malston@pivotal.io>
2017-09-08 17:40:16 -0600

    Use Model obj instead of a HashMap

--- src/main/java/com/example/app/web/IndexController.java
+++ src/main/java/com/example/app/web/IndexController.java
@@ -1,22 +1,21 @@
 package com.example.app.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Map;
 
 @Controller
 public class IndexController {
 
 	@GetMapping("/")
-	public String home(Map<String, Object> model, String message) {
-		model.put("message", message == null ? "Hello there!" : message);
+	public String home(Model model, String message) {
+		model.addAttribute("message", message == null ? "Hello there!" : message);
 		return "index";
 	}
 
 	@GetMapping("/next")
-	public String next(Map<String, Object> model) {
-		model.put("message", "Hello! You are in new page !!");
+	public String next(Model model) {
+		model.addAttribute("message", "Hello! You are in new page !!");
 		return "next";
 	}
 
