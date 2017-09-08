01e59f984cb8930d54d848481c2e47ec3d60effc
Mark Alston <malston@pivotal.io>
2017-09-08 16:04:15 -0600

    User newer GetMapping annotation

--- src/main/java/com/example/app/web/IndexController.java
+++ src/main/java/com/example/app/web/IndexController.java
@@ -1,27 +1,20 @@
 package com.example.app.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
 
 @Controller
 public class IndexController {
 
-//	@GetMapping("/")
-//	public String home(Map<String, Object> model, String message) {
-//		model.put("message", message == null ? "Hello there!" : message);
-//		return "index";
-//	}
-
-	@RequestMapping("/")
-	public ModelAndView home(@ModelAttribute("message") String message) {
-		return new ModelAndView("index", "message", message == null ? "Hello there!" : message);
+	@GetMapping("/")
+	public String home(Map<String, Object> model, String message) {
+		model.put("message", message == null ? "Hello there!" : message);
+		return "index";
 	}
 
-	@RequestMapping("/next")
+	@GetMapping("/next")
 	public String next(Map<String, Object> model) {
 		model.put("message", "Hello! You are in new page !!");
 		return "next";
